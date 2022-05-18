744bcfc9e09b4a877138319f9d88355ff698f123
lmartinresnick <lmartinresnick@gmail.com>
2022-05-17 22:10:23 -0700

    update(package): updated package

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftySwiftUI",
-            dependencies: []),
+            path: "Sources"),
         .testTarget(
             name: "SwiftySwiftUITests",
             dependencies: ["SwiftySwiftUI"]),
